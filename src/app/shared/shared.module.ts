fc73f96ee165ed1c79d16a902950318ce2d43c74
Luis Romo Portillo <luisrp9812@gmail.com>
2023-09-09 12:49:59 -0700

    Update: Add the imports and exports for shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,16 +4,9 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
-
-
 @NgModule({
-  declarations: [
-    NavBarComponent,
-    FooterComponent,
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NavBarComponent, FooterComponent, HeaderComponent],
+  imports: [CommonModule],
+  exports: [NavBarComponent, FooterComponent, HeaderComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
